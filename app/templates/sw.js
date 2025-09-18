9214115213104a948b8f23ab7dd45cca8097b503
mrtprd1824-collab <mr.tprd1824@gmail.com>
2025-09-18 19:32:42 +0700

    Update sw.js

--- app/templates/sw.js
+++ app/templates/sw.js
@@ -1,1 +1,18 @@
+self.addEventListener("push", function(event) {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: "/icon.png",   // ใช้ไฟล์ icon.png ที่อยู่ root
+    data: { url: data.url || "https://winner-line-bot-app.onrender.com/chat_all" }
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
 
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
